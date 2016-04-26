75a70a9cf5b2a13700e69936c2b8893fcecae73b
Hu Ziming <hzmangel@gmail.com>
2016-04-26 23:22:23 +0800

    Update ES index json fields

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,22 @@
+require 'elasticsearch/model'
+
 class Blog < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   belongs_to :author
+
+  def as_indexed_json(options = {})
+    as_json(
+      only: [
+        :title,
+        :content,
+        :created_at,
+        :updated_at,
+      ],
+      include: [
+        :author
+      ]
+    )
+  end
 end
